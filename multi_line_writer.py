11805868fe37e080c55af9113016b3124598a208
ChristianJames Luces <christianjames231103@gmail.com>
2023-04-23 18:35:48 +0800

    Pseudocode

---
+++ multi_line_writer.py
@@ -0,0 +1,6 @@
+# Write a method in python to write multiple line of text contents into a text file mylife.txt.
+
+# Open  the file in write mode
+# Ask the user to enter the lines of text
+# Write the line to the file
+# Ask teh user to enter more lines or stop
