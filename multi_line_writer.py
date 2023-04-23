315c2b5a704a982fa08f2cbe3f51d445064855ec
ChristianJames Luces <christianjames231103@gmail.com>
2023-04-23 21:50:18 +0800

    ask the input line from the user

--- multi_line_writer.py
+++ multi_line_writer.py
@@ -4,7 +4,8 @@
 with open("mylife.txt", "w") as my_file:
 # Create a while loop with a True condition
     while True:
-# Ask the input line from the user
+        # Ask the input line from the user
+        line = input("Enter Line: ")
 # Write the line to the file
 # Check if the user wants to input more lines
 # Exit the loop if the user doesn't want to add more lines
